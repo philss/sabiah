a1662240bf8f8f9251760357d20a0f6ebb90a2bc
Philip Sampaio Silva <philip.sampaio@gmail.com>
2016-09-10 20:03:21 -0300

    Remove actions we don't care about users registration

--- web/router.ex
+++ web/router.ex
@@ -17,7 +17,8 @@ defmodule Sabiah.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
-    resources "/users", UserController
+
+    resources "/users", UserController, only: [:index, :new, :create, :show]
   end
 
   # Other scopes may use custom stacks.
