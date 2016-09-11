d638ff49db828357f2461ef662efec76f4e97c6e
Philip Sampaio Silva <philip.sampaio@gmail.com>
2016-09-11 18:50:57 -0300

    Add validation about format and email
    Also remove options to edit and remove user.

--- web/models/user.ex
+++ web/models/user.ex
@@ -17,5 +17,7 @@ defmodule Sabiah.User do
     |> cast(params, [:name, :username, :email])
     |> validate_required([:name, :username, :email])
     |> validate_format(:email, ~r/@/)
+    |> validate_format(:username, ~r/^[a-zA-Z0-9_-]+$/)
+    |> validate_length(:username, min: 3, max: 42)
   end
 end
