799a6f0954e2adbb08a8a165fa68f45d550e55c6
Philip Sampaio Silva <philip.sampaio@gmail.com>
2016-09-12 00:44:20 -0300

    Working example of "solo" timeline

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule Sabiah.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", TimelineController, :index
+
     resources "/users",
               UserController,
               only: [:index, :new, :create, :show]
