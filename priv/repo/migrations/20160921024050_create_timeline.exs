07d7a06a90728a6399151aee3c5cf8c488990782
Philip Sampaio Silva <philip.sampaio@gmail.com>
2016-09-21 01:48:45 -0300

    Make broadcaster save the tweet in followers timeline
    This is needed in order to turn the tweet available at the user's
    timeline.
    It was needed to remove the timestamps of `timelines` in order to avoid
    to have to insert the timestamps at `Repo.insert_all`.
    
    https://hexdocs.pm/ecto/Ecto.Repo.html#c:insert_all/3

--- priv/repo/migrations/20160921024050_create_timeline.exs
+++ priv/repo/migrations/20160921024050_create_timeline.exs
@@ -6,7 +6,8 @@ defmodule Sabiah.Repo.Migrations.CreateTimeline do
       add :user_id, references(:users, on_delete: :nothing)
       add :tweet_id, references(:tweets, on_delete: :nothing)
 
-      timestamps()
+      # Disabled in order to turn `Repo.insert_all` more easy to use
+      # timestamps()
     end
     create index(:timelines, [:user_id])
     create index(:timelines, [:tweet_id])
--- web/channels/tweet_broadcaster.ex
+++ web/channels/tweet_broadcaster.ex
@@ -4,14 +4,25 @@ defmodule Sabiah.TweetBroadcaster do
   import Ecto.Query
 
   def broadcast_to_followers(tweet, payload) do
-    followers_ids_query = from f in "followers",
-                            where: f.followed_user_id == ^tweet.user_id,
-                            select: f.user_id
-
-    follower_ids = Repo.all(followers_ids_query)
+    follower_ids = follower_user_ids(tweet)
+    Repo.insert_all("timelines", timelines(tweet, follower_ids))
 
     Enum.map(follower_ids, fn(user_id) ->
       Endpoint.broadcast!("timeline:#{user_id}", "new_tweet", payload)
     end)
+  end
+
+  defp timelines(tweet, follower_ids) do
+    Enum.map([tweet.user_id|follower_ids], fn(user_id) ->
+      [user_id: user_id, tweet_id: tweet.id]
+    end)
+  end
+
+  defp follower_user_ids(tweet) do
+    followers_ids_query = from f in "followers",
+                            where: f.followed_user_id == ^tweet.user_id,
+                            select: f.user_id
+
+    Repo.all(followers_ids_query)
   end
 end
