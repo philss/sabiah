793809d6f441a3a38528b94e33440a7c388a5349
Philip Sampaio Silva <philip.sampaio@gmail.com>
2016-09-21 00:18:58 -0300

    Improve readability of broadcaster

--- web/channels/tweet_broadcaster.ex
+++ web/channels/tweet_broadcaster.ex
@@ -7,9 +7,10 @@ defmodule Sabiah.TweetBroadcaster do
     followers_ids_query = from f in "followers",
                             where: f.followed_user_id == ^tweet.user_id,
                             select: f.user_id
-    followers_ids_query
-    |> Repo.all
-    |> Enum.map(fn(user_id) ->
+
+    follower_ids = Repo.all(followers_ids_query)
+
+    Enum.map(follower_ids, fn(user_id) ->
       Endpoint.broadcast!("timeline:#{user_id}", "new_tweet", payload)
     end)
   end
