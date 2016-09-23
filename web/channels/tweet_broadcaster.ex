fd69a2715f2fc0ff14372d9c337b75590d480466
Philip Sampaio Silva <philip.sampaio@gmail.com>
2016-09-23 01:37:06 -0300

    Better naming

--- web/channels/tweet_broadcaster.ex
+++ web/channels/tweet_broadcaster.ex
@@ -4,21 +4,21 @@ defmodule Sabiah.TweetBroadcaster do
   import Ecto.Query
 
   def broadcast_to_followers(tweet, payload) do
-    follower_ids = follower_user_ids(tweet)
+    followers_ids = follower_user_ids(tweet)
 
     # You can insert multiple values at once.
     # It translate into `INSERT INTO timelines(user_id, tweet_id) VALUES (1, 2), (2, 1);`
-    Repo.insert_all("timelines", timelines(tweet, follower_ids))
+    Repo.insert_all("timelines", timelines(tweet, followers_ids))
 
-    Enum.map(follower_ids, fn(user_id) ->
+    Enum.map(followers_ids, fn(user_id) ->
       Endpoint.broadcast!("timeline:#{user_id}", "new_tweet", payload)
     end)
   end
 
   # Here we also add the tweet author to the list of timelines that
   # gonna be saved.
-  defp timelines(tweet, follower_ids) do
-    Enum.map([tweet.user_id|follower_ids], fn(user_id) ->
+  defp timelines(tweet, followers_ids) do
+    Enum.map([tweet.user_id|followers_ids], fn(user_id) ->
       [user_id: user_id, tweet_id: tweet.id]
     end)
   end
